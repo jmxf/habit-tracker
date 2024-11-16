d200b241fbea5bcc6aa21409c66e55733fb49e57
Jessica <j.moxam-fangmann@outlook.com>
2024-11-16 18:15:06 +0000

    Add basic inputs

--- app.R
+++ app.R
@@ -5,20 +5,27 @@ ui <- fluidPage(
 
     # Application title
     titlePanel("Habit Tracker"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-        )
-    )
+    
+    selectInput("category", "Category", choices = c("Symptoms", "Chores")),
+    selectInput("activity", "Activity", choices = c("Mood", "Headache", "Laundry")),
+    dateInput("dateTracked", "Date", format = "dd.mm.yyyy", weekstart = 1),
+    textInput("activityNote", "Notes"),
+    actionButton("save", "Save", class = "btn-primary"),
+    
+    textOutput("recordMessage")
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
+  
+  output$recordMessage <- renderText({
+    paste0(c("You recorded: ", input$category))
+  })
+  
+  observeEvent(input$save, {
+    message("New entry saved!")
+  })
+
 }
 
 # Run the application 
